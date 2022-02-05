bc25bc0fa1a54693c87ffb478ddd946b972bd284
John Reich <55339121+teelrabbit@users.noreply.github.com>
2022-02-05 03:29:50 -0800

    Update soul-storage.sol

--- mod·ule_1/soul-storage.sol
+++ mod·ule_1/soul-storage.sol
@@ -1,1 +1,11 @@
+pragma solidity ^0.6.0;
 
+contract soulStorage {
+
+    uint256 exampleNum; //NULL 
+
+    function store(uint256 _exampleNum) public {
+        exampleNum = _exampleNum;
+    }
+
+}
