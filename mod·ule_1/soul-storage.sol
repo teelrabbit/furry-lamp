04330cd2205316be875e0e9247d72bbd02a77106
xuntitled <xuntitled@protonmail.com>
2022-02-05 04:08:11 -0800

    added notes

--- mod·ule_1/soul-storage.sol
+++ mod·ule_1/soul-storage.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: UNLICENSED;
-pragma solidity ^0.6.0;
+pragma solidity >=0.6.0 <0.9.0;
 
 contract soulStorage {
 
@@ -8,5 +8,10 @@ contract soulStorage {
     function store(uint256 _exampleNum) public {
         exampleNum = _exampleNum;
     }
-
+    // view means, we are just reading from the blockchain 
+    function retrieve() public view returns(uint256) { // function named retrieve, that is public 
+                                                       // view, then it states that it returns an unsigned int of 256 bytes 
+        return exampleNum;
+    }
 }
+
