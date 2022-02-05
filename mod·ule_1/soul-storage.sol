0d452afe857fb5aafc997104fc8740dc11b4656d
John Reich <55339121+teelrabbit@users.noreply.github.com>
2022-02-05 03:40:59 -0800

    Update soul-storage.sol

--- mod·ule_1/soul-storage.sol
+++ mod·ule_1/soul-storage.sol
@@ -1,8 +1,9 @@
+//SPDX-License-Identifier: UNLICENSED;
 pragma solidity ^0.6.0;
 
 contract soulStorage {
 
-    uint256 exampleNum; //NULL 
+    uint256 public exampleNum; //NULL 
 
     function store(uint256 _exampleNum) public {
         exampleNum = _exampleNum;
