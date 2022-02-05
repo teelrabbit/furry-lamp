b23e635045bc9c3c91eac1eb0fad2b028d7551d9
teelrabbit <xuntitled@protonmail.com>
2022-02-05 04:51:23 -0800

    struct notes

--- mod·ule_1/soul-storage.sol
+++ mod·ule_1/soul-storage.sol
@@ -3,8 +3,17 @@ pragma solidity >=0.6.0 <0.9.0;
 
 contract soulStorage {
 
-    uint256 public exampleNum; //NULL 
+    uint256 public exampleNum; //NULL //Public making it global 
 
+    struct people {
+        uint exampleNum;
+        string name;
+    } 
+
+    people public person = people({exampleNum: 4, name: "tyler"});
+    people public animal = people({exampleNum: 8, name: "john"});
+    //name of struct, seting it to public, an object named person
+    //calling the struct "people", then definning the ojects inside the struct :D
     function store(uint256 _exampleNum) public {
         exampleNum = _exampleNum;
     }
